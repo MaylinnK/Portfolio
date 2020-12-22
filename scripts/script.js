b2eb570de29f600a2a4ec8510314ae4be993a590
MaylinnK <74138768+MaylinnK@users.noreply.github.com>
2020-12-22 17:35:20 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser * /
+
+/*eslint 'no-console': 0*/
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+document.querySelectorAll('a[href^="#works"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
